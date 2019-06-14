ba0a7555f73800962244b3dba8b21f0b1e7871db
Luis Gustavo Rosa Cardoso <44246590+LuisGustavoRosa@users.noreply.github.com>
2019-06-13 21:41:42 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 $(document).ready(function () {
     $("#home").click(function () {
-        $("section").load('home.html');
+        $("#section").load('home.html');
     });
 
     $("#fotos").click(function () {
-        $("section").load('fotos.html');
+        $("#section").load('fotos.html');
     });
 
     $("#cadastro").click(function () {
-        $("section").load('cadastro.html');
+        $("#section").load('cadastro.html');
     });
 
     $("#sobre").click(function () {
-        $("section").load('sobre.html');
+        $("#section").load('sobre.html');
     });
 });
 
